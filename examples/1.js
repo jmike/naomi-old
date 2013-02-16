62b5084e11708f3c590803deaa200a963a0a8417
Dimitrios C. Michalakos <dimitris@jmike.gr>
2013-02-16 16:40:07 +0200

    Updated the example to use the updated Naomi API.

--- examples/1.js
+++ examples/1.js
@@ -1,20 +1,21 @@
-var Naomi = require("../")
+var Naomi = require("../");
+var Attribute = Naomi.Attribute;
 
-var naomi = new Naomi("MYSQL", {
+var db = new Naomi("MYSQL", {
 	database: "test",
 	username: "root",
 	password: "",
 	host: "127.0.0.1"
 });
-//naomi.extend("user", {
-//	first_name: 
-//})
 
-//User = Naomi.extend("user", {
-//	"name": new StringAttribute.maxLength(100)
-//	"email": new EmailAttribute.nullable().maxLength(150)
-//}, {
-//	engine: Naomi.MYISAM
-//})
+var User = db.extend("user", {
+	"name": Attribute.integer().max(100),
+	"email": Attribute.boolean()
+}, {
+	engine: Naomi.MYISAM
+});
+
+db.sync();
+
 //User.fetch(Query.filter(["id", "=", 10]).order("name").offset(5).limit(1), callback)
 //User.fetch().where(["id", "=", 10]).order("name").offset(5).limit(1).execute(callback)
